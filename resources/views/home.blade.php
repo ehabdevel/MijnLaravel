200de333c85c69319ea9ae9fd13563428b637cf9
Ehab <ehabdevel@gmail.com>
2017-04-18 05:08:11 +0200

    Upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,209 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>MijnLaravel</title>
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<!--[if lt IE 9]>
+<script src="{{ URL::asset('../_scripts/respond.min.js') }}"></script>
+<![endif]-->
+<link href="{{ URL::asset('_css/main.css') }}" rel="stylesheet" media="screen, projection">
+<script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+</head>
+<body>
+<header class="aboutHeader pageHeader">
+<h1>MijnLaravel Theme of Art and Design<a href="#" title="home"></a></h1>
+<nav id="pageNav" class="cf">
+  <ul>
+    <li><a href="#" title="programs">Programs</a></li>
+    <li><a href="#" title="admissions">Admissions</a></li>
+    <li><a href="#" title="student portal">Student Portal</a></li>
+    <li><a href="#" title="campus portal">Campus</a></li>
+    <li><a href="#" title="alumni">Alumni</a></li>
+    <li><a href="#"  title="about MijnLaravel Theme">About</a></li>
+  </ul>
+</nav>
+</header>
+<article id="mainContent">
+  <h1>MijnLaravel Theme</h1>
+
+  <p>We&rsquo;ve been providing fine art and design education for over 150 years. But age hasn&rsquo;t slowed us down. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia et pariatur ipsam tempora officia ea iusto expedita, nulla, hic odit saepe repellat nesciunt dolorum, officiis laborum ad, aliquam. Quos, et.</p>
+  <p>You'll find the supportive environment at MijnLaravel encourages collaboration and creates enthusiasm among Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia et pariatur ipsam tempora officia ea iusto expedita, nulla, hic odit saepe repellat nesciunt dolorum, officiis laborum ad, aliquam. Quos, et.</p>
+  <h2>Our continuing mission</h2>
+  <p>At MijnLaravel, our mission is to teach and inspire the next generation&rsquo;s artists to create work that changes the way people think, and hopefully, even the way they act. We believe art inspires compassion by providing audiences with an empathetic outlet. We believe art informs and can advertise important messages. We believe it is our duty to show artists how to best use their tools and best communicate their message across. <strong>We exist to educate</strong>.</p>
+  <h2>Map &amp; Directions</h2>
+  <section class="address">
+<h3>MijnLaravel Theme</h3>
+<p>824 West 33rd Street<br>
+Hurghada, Cairo<br>
+Egypt</p>
+</section>
+  <div id="mapCanvas" style="width:381px; height:330px; min-width:300px; min-height:300px"></div>
+  <h2>Transportation</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia et pariatur ipsam tempora officia ea iusto expedita, nulla, hic odit saepe repellat nesciunt dolorum, officiis laborum ad, aliquam. Quos, et.
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia et pariatur ipsam tempora officia ea iusto expedita, nulla, hic odit saepe repellat nesciunt dolorum, officiis laborum ad, aliquam. Quos, et.</p>
+</article>
+<aside>
+<section class="academic info">
+<h2>Academic Links</h2>
+<ul>
+  <li><a href="#" title="undergraduate degrees">Undergraduate Degrees</a></li>
+  <li><a href="#" title="Graduate Degrees">Graduate Degree Programs</a></li>
+  <li><a href="#" title="Continuing education">Continuing Education</a></li>
+  <li><a href="#" title="Faculty">Faculty Profiles</a></li>
+   <li><a href="#" title="Application requirements">Application Requirements</a></li>
+  <li><a href="#" title="Study abroad programs">Study Abroad Programs</a></li>
+  <li><a href="#" title="Internship programs">Internship Programs</a></li>
+  <li><a href="#" title="Academic calendar">Academic Calendar</a></li>
+</ul>
+</section>
+<section class="connect info">
+<h2>Connect with us</h2>
+<ul>
+  <li><a href="http://www.twitter.com/" title="Follow us!" target="_blank" class="twitter">Twitter</a></li>
+  <li><a href="https://plus.google.com/" title="Google Plus" target="_blank" class="google">Google Plus</a></li>
+  <li><a href="http://facebook.com" title="Be our friend" target="_blank" class="facebook">Facebook</a></li>
+  <li><a href="http://www.linkedin.com" title="Let's connect" target="_blank" class="linked">LinkedIn</a></li>
+</ul>
+</section>
+<section class="find info">
+<h2>Find out more</h2>
+<ul>
+<li><a href="#" title="Take a campus tour">Take a campus tour</a></li>
+<li><a href="#" title="course catalog">Download a course catalog</a></li>
+<li><a href="#" title="Apply now">Apply now</a></li>
+</ul>
+</section>
+<section class="show info">
+<h2>Current Show</h2>
+<img src="{{ URL::asset('_images/show1.jpg') }}" width="285" height="230" alt="Current show">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia et pariatur ipsam tempora officia ea iusto expedita, nulla, hic odit saepe repellat nesciunt dolorum, officiis laborum ad, aliquam. Quos, et. <a href="#" title="current show" class="more">more info</a></p>
+</section>
+</aside>
+<footer id="pageFooter" class="cf">
+<nav class="footerNav">
+<section class="col">
+<h3>About MijnLaravel Theme</h3>
+  <div class="col1">
+  <ul>
+  <li><a href="#" title="Our mission">Mission Statement</a></li>
+  <li><a href="#" title="school history">School History</a></li>
+  <li><a href="#" title="accreditation and affliates">Accreditation &amp; Affiliates</a></li>
+  <li><a href="#" title="board members">Board Members</a></li>
+  </ul>
+  </div>
+  <div class="col2">
+  <ul>
+  <li><a href="#" title="faculty and staff">Faculty &amp; Staff</a></li>
+  <li><a href="#" title="visiting professors">Visiting Professors</a></li>
+  <li><a href="#" title="Maribielle MijnLaravel Museum">Marbielle MijnLaravel Museum</a></li>
+  <li><a href="#" title="directions">Map &amp; Directions</a></li>
+  </ul>
+  </div>
+</section>
+<section class="col">
+<h3>Admissions &amp; Programs</h3>
+  <div class="col1">
+  <ul>
+  <li><a href="#" title="Degree programs">Degree Programs</a></li>
+  <li><a href="#" title="course catalog">Course Catalog</a></li>
+  <li><a href="#" title="fine art programs">Fine Art Programs</a></li>
+  <li><a href="#" title="design concentration">Design Concentration</a></li>
+  </ul>
+  </div>
+  <div class="col2">
+  <ul>
+  <li><a href="#" title="fashion program">Fashion Program</a></li>
+  <li><a href="#" title="product design">Product Design</a></li>
+  <li><a href="#" title="Continuing Education">Continuing Education</a></li>
+  <li><a href="#" title="tuition and financial aid">Tuition &amp; Financial Aid</a></li>
+  </ul>
+  </div>
+</section>
+<section class="col">
+<h3>Student Resources</h3>
+  <div class="col1">
+  <ul>
+  <li><a href="#" title="Campus information">Campus Information</a></li>
+  <li><a href="#" title="student housing">Student Housing</a></li>
+  <li><a href="#" title="transcript request">Transcript Request</a></li>
+  <li><a href="#" title="application process">Application Process</a></li>
+  </ul>
+  </div>
+  <div class="col2">
+  <ul>
+  <li><a href="#" title="portfolio review">Portfolio Review</a></li>
+  <li><a href="#" title="career counselling">Career Counselling</a></li>
+  <li><a href="#" title="internship programs">Internship Programs</a></li>
+  <li><a href="#" title="student portal login">Student Portal Login</a></li>
+   </ul>
+  </div>
+</section>
+</nav>
+<p>&copy;Copyright  MijnLaravel Theme of Art &amp; Design.  All rights reserved.<a href="#" title="privacy statement"> Privacy Statement</a> |<a href="#" title="legal terms"> Legal Terms and Conditions</a> |<a href="#" title="student outcomes and disclosures"> Student Outcomes/Disclosures</a></p>
+</footer>
+</body>
+ <script type="text/javascript">
+// BeginOAWidget_Instance_2187524: #mapCanvas
+
+	  // initialize the google Maps
+
+     function initializeGoogleMap() {
+		// set latitude and longitude to center the map around
+		var latlng = new google.maps.LatLng(52.2998248,4.9442962);
+
+		// set up the default options
+		var myOptions = {
+		  zoom: 15,
+		  center: latlng,
+		  navigationControl: true,
+		  navigationControlOptions:
+		  	{style: google.maps.NavigationControlStyle.DEFAULT,
+			 position: google.maps.ControlPosition.TOP_LEFT },
+		  mapTypeControl: true,
+		  mapTypeControlOptions:
+		  	{style: google.maps.MapTypeControlStyle.DEFAULT,
+			 position: google.maps.ControlPosition.TOP_RIGHT },
+
+		  scaleControl: true,
+		   scaleControlOptions: {
+        		position: google.maps.ControlPosition.BOTTOM_LEFT
+    	  },
+		  mapTypeId: google.maps.MapTypeId.ROADMAP,
+		  draggable: true,
+		  disableDoubleClickZoom: false,
+		  keyboardShortcuts: true
+		};
+		var map = new google.maps.Map(document.getElementById("mapCanvas"), myOptions);
+		if (false) {
+			var trafficLayer = new google.maps.TrafficLayer();
+			trafficLayer.setMap(map);
+		}
+		if (false) {
+			var bikeLayer = new google.maps.BicyclingLayer();
+			bikeLayer.setMap(map);
+		}
+		if (true) {
+			addMarker(map,52.2998248,4.9442962,"MijnLaravel Theme");
+		}
+	  }
+
+	  window.onload = initializeGoogleMap();
+
+	 // Add a marker to the map at specified latitude and longitude with tooltip
+	 function addMarker(map,lat,long,titleText) {
+	  	var markerLatlng = new google.maps.LatLng(lat,long);
+	 	var marker = new google.maps.Marker({
+      		position: markerLatlng,
+      		map: map,
+      		title:"MijnLaravel Theme",
+			icon: ""});
+	 }
+
+
+// EndOAWidget_Instance_2187524
+  </script>
+</html>
